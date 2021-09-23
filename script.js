f2c2f4eb93829e860c7e53ace04f925bd0953a92
unknown <Quentinhnilica@gmail.com>
2021-09-22 22:42:15 -0400

    Changed out resume modal for a link to my resume on google Drive

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const contactMod = document.querySelector('#contactModal')
 const contactButton = document.querySelector('#contact')
 const closeMod = document.querySelector('#closeMod')
-const resumeMod = document.querySelector('#resumeModal')
-const resumeButton = document.querySelector('#resume')
-const closeResume = document.querySelector('#closeResume')
 
 function openMod(e){
     let thisTarget = document.querySelector('#' + e.target.id + "Modal") 
@@ -14,11 +11,5 @@ function closeContact(e){
     contactMod.classList.remove('is-active')
 }
 
-function closeResumeMod(e){
-    resumeMod.classList.remove('is-active')
-}
-
 closeMod.addEventListener('click', closeContact)
 contactButton.addEventListener('click', openMod)
-resumeButton.addEventListener('click', openMod)
-closeResume.addEventListener('click', closeResumeMod)
